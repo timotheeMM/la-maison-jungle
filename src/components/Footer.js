83f3bf1d16f2bd3d0d821ad35a37c519c4e6f0f5
timotheeMM <143833750+timotheeMM@users.noreply.github.com>
2024-09-08 16:07:16 +0200

    configure prettier and format code

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,33 +1,35 @@
-import { useState } from 'react'
-import '../styles/Footer.css'
+import { useState } from 'react';
+import '../styles/Footer.css';
 
 function Footer() {
-	const [inputValue, setInputValue] = useState('')
+  const [inputValue, setInputValue] = useState('');
 
-	function handleInput(e) {
-		setInputValue(e.target.value)
-	}
+  function handleInput(e) {
+    setInputValue(e.target.value);
+  }
 
-	function handleBlur() {
-		if (!inputValue.includes('@')) {
-			alert("Attention, il n'y a pas d'@, ceci n'est pas une adresse valide 😥")
-		}
-	}
+  function handleBlur() {
+    if (!inputValue.includes('@')) {
+      alert(
+        "Attention, il n'y a pas d'@, ceci n'est pas une adresse valide 😥",
+      );
+    }
+  }
 
-	return (
-		<footer className='lmj-footer'>
-			<div className='lmj-footer-elem'>
-				Pour les passionné·e·s de plantes 🌿🌱🌵
-			</div>
-			<div className='lmj-footer-elem'>Laissez-nous votre mail :</div>
-			<input
-				placeholder='Entrez votre mail'
-				onChange={handleInput}
-				value={inputValue}
-				onBlur={handleBlur}
-			/>
-		</footer>
-	)
+  return (
+    <footer className="lmj-footer">
+      <div className="lmj-footer-elem">
+        Pour les passionné·e·s de plantes 🌿🌱🌵
+      </div>
+      <div className="lmj-footer-elem">Laissez-nous votre mail :</div>
+      <input
+        placeholder="Entrez votre mail"
+        onChange={handleInput}
+        value={inputValue}
+        onBlur={handleBlur}
+      />
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
