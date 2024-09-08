c84494124672c62a55b3db7e1ab412d2a21932f6
timotheeMM <143833750+timotheeMM@users.noreply.github.com>
2024-09-08 16:16:33 +0200

    improve the user experience

--- src/components/PlantItem.js
+++ src/components/PlantItem.js
@@ -7,9 +7,14 @@ function handleClick(plantName) {
 
 function PlantItem({ cover, name, water, light, price }) {
   return (
-    <li className="lmj-plant-item" onClick={() => handleClick(name)}>
+    <li className="lmj-plant-item">
       <span className="lmj-plant-item-price">{price}€</span>
-      <img className="lmj-plant-item-cover" src={cover} alt={`${name} cover`} />
+      <img
+        className="lmj-plant-item-cover"
+        src={cover}
+        alt={`${name} cover`}
+        onClick={() => handleClick(name)}
+      />
       {name}
       <div>
         <CareScale careType="water" scaleValue={water} />
